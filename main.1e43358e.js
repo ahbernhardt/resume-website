df7a225d3e2358544db9f10dc626b715875692e8
Anh Nguyen <a.nguyen0208@gmail.com>
2020-11-13 21:14:52 -0600

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -5547,7 +5547,7 @@ var indexLaunch = function indexLaunch() {
     duration: 2,
     scaleY: 0,
     ease: 'power4.inOut'
-  }).fromTo('.resume', {
+  }).fromTo('nav a', {
     y: '60%',
     autoAlpha: 0
   }, {
@@ -5556,24 +5556,26 @@ var indexLaunch = function indexLaunch() {
     duration: 2,
     ease: 'power4.out',
     stagger: 0.2
-  }, '-=1.2').fromTo('.logo', {
+  }, '-=1.5').fromTo('.logo', {
     y: '60%',
     autoAlpha: 0
   }, {
     autoAlpha: 1,
     y: '0%',
-    duration: 1.6,
-    ease: 'power4.out'
-  }, '-=2').fromTo('.project', {
-    x: 15,
+    duration: 2,
+    ease: 'power4.out',
+    stagger: 0.2
+  }, '-=1.5').fromTo('.resume', {
+    y: '60%',
     autoAlpha: 0
   }, {
     autoAlpha: 1,
-    x: 0,
-    duration: 1.6,
-    stagger: 0.08,
-    ease: 'power4.out'
-  }, '-=1.8').fromTo('.section-line', {
+    y: '0%',
+    duration: 2,
+    ease: 'power4.out',
+    stagger: 0.2
+  }, '-=1.2') // .fromTo('.project', { x: 15, autoAlpha: 0 }, { autoAlpha: 1, x: 0, duration: 1.6, stagger: 0.08, ease: 'power4.out' }, '-=1.8')
+  .fromTo('.section-line', {
     scaleY: 0,
     autoAlpha: 0
   }, {
@@ -5622,7 +5624,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58384" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59119" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
