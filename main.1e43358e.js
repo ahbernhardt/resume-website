e37ae16b97ba13a94322570be9ad38ccaffcb24b
Anh Nguyen <a.nguyen0208@gmail.com>
2020-11-13 21:51:46 -0600

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -5624,7 +5624,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59794" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64741" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
